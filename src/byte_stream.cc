c1dcac10d1cb6c5ced6e13c4c135d4ab786ef8bf
Zari Tsu <zaritsu030907@gmail.com>
2024-05-29 18:27:14 +0800

    byte_stream v1

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -6,61 +6,71 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 bool Writer::is_closed() const
 {
-  	// Your code here.
-	
-  	return {};
+  	return is_close_;
 }
 
 void Writer::push( string data )
 {
-  	// Your code here.
-  	(void)data;
-  	return;
+  	buf_.push_back(data);
+	bytePushed_ += data.size();
 }
 
 void Writer::close()
 {
-  	// Your code here.
+  	is_close_ = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  	// Your code here.
-  	return {};
+	uint64_t size = 0;
+	for (const string& s : buf_) {
+		size += s.size();
+	}
+  	return (capacity_ - size);
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  	// Your code here.
-  	return {};
+  	return bytePushed_;
 }
 
 bool Reader::is_finished() const
 {
-  	// Your code here.
-  	return {};
+  	return bytePushed_ == bytePoped_;
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  	// Your code here.
-  	return {};
+  	return bytePoped_;
 }
 
 string_view Reader::peek() const
 {
-	// Your code here.
-  	return {};
+  	return string_view(buf_[0]);
 }
 
 void Reader::pop( uint64_t len )
 {
-  	// Your code here.
- 	(void)len;
+ 	uint64_t cnt = 0;
+	for (;;) {
+		uint64_t size = buf_[0].size();
+		if (cnt == len) {
+			break;
+		}
+		if (cnt + size < len) {
+			buf_.erase(buf_.begin());
+		} else {
+			buf_[0] = buf_[0].substr(len - cnt);
+			break;
+		}
+	}
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-    // Your code here.
-    return {};
+	uint64_t size = 0;
+    for (const string& s : buf_) {
+		size += s.size();
+	}
+	return size;
 }
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <string>
 #include <string_view>
+#include <queue>
 
 class Reader;
 class Writer;
@@ -25,6 +26,10 @@ public:
     // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
     uint64_t capacity_;
     bool error_ {};
+    bool is_close_ {};
+    uint64_t bytePushed_ {};
+    uint64_t bytePoped_ {};
+    std::vector<std::string> buf_;
 };
 
 class Writer : public ByteStream
