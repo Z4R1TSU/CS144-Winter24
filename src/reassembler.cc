c721b379daf3995a042d3349f76e70e67daa4a06
Zari Tsu <zaritsu030907@gmail.com>
2024-06-01 10:43:29 +0800

    formating the indent of reassembler

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -4,14 +4,14 @@ using namespace std;
 
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
-  // Your code here.
-  (void)first_index;
-  (void)data;
-  (void)is_last_substring;
+    // Your code here.
+    (void)first_index;
+    (void)data;
+    (void)is_last_substring;
 }
 
 uint64_t Reassembler::bytes_pending() const
 {
-  // Your code here.
-  return {};
+    // Your code here.
+    return {};
 }
