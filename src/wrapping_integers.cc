29dcdaca265255a810dd1730bc83009319cf8b1f
Zari Tsu <zaritsu030907@gmail.com>
2024-06-04 11:28:13 +0800

    wrap passed but unwrap failed

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,25 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-	// Your code here.
-	(void)n;
-	(void)zero_point;
-	return Wrap32 { 0 };
+	uint32_t raw = (static_cast<uint32_t>(n) + zero_point.raw_value_) % (1UL << 32);
+	return Wrap32 { raw };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-	// Your code here.
-	(void)zero_point;
-	(void)checkpoint;
-	return {};
+	uint64_t prefix = checkpoint & 0xFFFF'FFFF'0000'0000;
+	// uint32_t raw = (this->raw_value_ - zero_point.raw_value_) % (1UL << 32);
+	// uint64_t probable_res2 = prefix | (static_cast<uint64_t>(raw));
+	uint64_t raw = (static_cast<uint64_t>(this->raw_value_) - static_cast<uint64_t>(zero_point.raw_value_));
+	uint64_t probable_res2 = prefix + raw;
+	uint64_t probable_res1 = probable_res2 - (1UL << 32);
+	uint64_t probable_res3 = probable_res2 + (1UL << 32);
+
+	if (checkpoint - probable_res1 < (1UL << 31)) {
+		return probable_res1;
+	} else if (probable_res3 - checkpoint < (1UL << 31)) {
+		return probable_res3;
+	} else {
+		return probable_res2;
+	}
 }
