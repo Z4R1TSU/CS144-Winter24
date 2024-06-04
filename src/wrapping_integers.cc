dce075aab3a8600b5d39ff53ed19a8218170f0c9
Zari Tsu <zaritsu030907@gmail.com>
2024-06-04 10:23:23 +0800

    format the lab2 wrapping

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,16 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+	// Your code here.
+	(void)n;
+	(void)zero_point;
+	return Wrap32 { 0 };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+	// Your code here.
+	(void)zero_point;
+	(void)checkpoint;
+	return {};
 }
