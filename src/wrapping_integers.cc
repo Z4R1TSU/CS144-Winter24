95447abd1c4d7685a86a4d4627c12e0f74d9e24f
Zari Tsu <zaritsu030907@gmail.com>
2024-06-04 11:34:28 +0800

    pass wrap and unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -11,12 +11,15 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
 	uint64_t prefix = checkpoint & 0xFFFF'FFFF'0000'0000;
-	// uint32_t raw = (this->raw_value_ - zero_point.raw_value_) % (1UL << 32);
-	// uint64_t probable_res2 = prefix | (static_cast<uint64_t>(raw));
-	uint64_t raw = (static_cast<uint64_t>(this->raw_value_) - static_cast<uint64_t>(zero_point.raw_value_));
+	uint64_t raw;
+	if (this->raw_value_ >= zero_point.raw_value_) {
+		raw = (static_cast<uint64_t>(this->raw_value_) - static_cast<uint64_t>(zero_point.raw_value_));
+	} else {
+		raw = (static_cast<uint64_t>(this->raw_value_) + (1UL << 32) - static_cast<uint64_t>(zero_point.raw_value_));
+	}
 	uint64_t probable_res2 = prefix + raw;
-	uint64_t probable_res1 = probable_res2 - (1UL << 32);
-	uint64_t probable_res3 = probable_res2 + (1UL << 32);
+	uint64_t probable_res1 = probable_res2 > (1UL << 32) ? probable_res2 - (1UL << 32) : probable_res2;
+	uint64_t probable_res3 = probable_res2 < (0xFFFF'FFFF'FFFF'FFFF) - (1UL << 32) ? probable_res2 + (1UL << 32) : probable_res2;
 
 	if (checkpoint - probable_res1 < (1UL << 31)) {
 		return probable_res1;
