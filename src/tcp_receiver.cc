211af56543e0370fab6f2cbd38257ceca58fdd34
Zari Tsu <zaritsu030907@gmail.com>
2024-06-04 17:14:02 +0800

    tcp_receiver: passed all tests

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -11,15 +11,17 @@ void TCPReceiver::receive( TCPSenderMessage message )
 		reader().set_error();
 		return;
 	}
-	// void insert( uint64_t first_index, std::string data, bool is_last_substring );
 	// the parameters should be translated from seqno to absolute seqno
-	if (!this->isn_.has_value()) {
-		isn_.emplace(message.seqno + static_cast<uint32_t>(message.SYN));
+	if (!isn_.has_value()) {
+		if (!message.SYN) {
+			return;
+		}
+		isn_.emplace(message.seqno);
 	}
 	Wrap32 zero_point = isn_.value();
-	uint64_t checkpoint = writer().bytes_pushed();
+	uint64_t checkpoint = writer().bytes_pushed() + static_cast<uint32_t>(message.SYN);
 	uint64_t abs_seqno = message.seqno.unwrap(zero_point, checkpoint);
-	uint64_t stream_index = abs_seqno - static_cast<uint64_t>(message.SYN);
+	uint64_t stream_index = abs_seqno + static_cast<uint64_t>(message.SYN) - 1;
 	reassembler_.insert(stream_index, move(message.payload), message.FIN);
 }
 
@@ -28,7 +30,8 @@ TCPReceiverMessage TCPReceiver::send() const
 	uint16_t wdsz = static_cast<uint16_t>(min(writer().available_capacity(), static_cast<uint64_t>(UINT16_MAX)));
 	bool reset = writer().has_error();
 	if (isn_.has_value()) {
-		return { isn_.value(), wdsz, reset };
+		Wrap32 ackno = Wrap32::wrap(writer().bytes_pushed() + static_cast<uint64_t>(writer().is_closed()), isn_.value()) + 1;
+		return { ackno, wdsz, reset };
 	} else {
 		return { nullopt, wdsz, reset };
 	}
